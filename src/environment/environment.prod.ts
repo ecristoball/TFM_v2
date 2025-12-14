0bd6f8cd23d4cfa758c800ee917867489b1c49a8
ecristoball <ecristoball@uoc.edu>
2025-12-14 22:43:14 +0100

    depurar 4

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tfmv2back-production.up.railway.app/api'
+    apiUrl: 'https://tfmv2back-production.up.railway.app/app/api'
+  //apiUrl: 'https://tfmv2back-production.up.railway.app/api'
 };
