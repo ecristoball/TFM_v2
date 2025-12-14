0d1c092d31aef65ce8ee1796517b5a5713b431ed
ecristoball <ecristoball@uoc.edu>
2025-12-15 00:14:20 +0100

    depurar 4.10

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,8 +1,15 @@
 
-
+/*
 // environment.ts
 export const environment = {
   production: false,
+  apiUrl: window.location.hostname.includes('netlify.app')
+    ? 'https://tfmv2back-production.up.railway.app/api'
+    : 'http://127.0.0.1:8000/api'
+};*/
+// environment.ts
+export const environment = {
+  production: window.location.hostname.includes('netlify.app'),
   apiUrl: window.location.hostname.includes('netlify.app')
     ? 'https://tfmv2back-production.up.railway.app/api'
     : 'http://127.0.0.1:8000/api'
